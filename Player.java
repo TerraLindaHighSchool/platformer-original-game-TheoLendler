8184909333e95342b968d7873f832cc747cd4f05
TheoLendler <tl42546@srcs.org>
2021-09-23 17:41:32 -0500

    9-23-21

--- Player.java
+++ Player.java
@@ -8,6 +8,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Player extends Actor
 {
+    private Health Health[];
+    private PowerUp Powerup[];
+    private int HealthCount;
+    private int Speed;
+    private int WalkIndex;
+    private int Frame;
+    private float yVelocity;
+    private boolean IsWalking;
+    
     /**
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
